fbc725d59ddd35c56a267f3cff4aeb0fd2d2d4db
AmineSidki <amineyoutubw@gmail.com>
2026-01-17 02:37:15 +0100

    First REAL commit, it generates repositories already hehe

---
+++ Sprout/src/main/java/org/AmineSidki/App.java
@@ -0,0 +1,12 @@
+package org.AmineSidki;
+
+import org.AmineSidki.runnable.DefaultRunnable;
+import picocli.CommandLine;
+
+public class App
+{
+    public static void main( String[] args ){
+        int exitCode = new CommandLine(new DefaultRunnable()).execute(args);
+        System.exit(exitCode);
+    }
+}
