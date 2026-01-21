176ca87acccc68f4a29d52af2b109fff1491e20b
AmineSidki <amineyoutubw@gmail.com>
2026-01-21 12:38:37 +0100

    temp state

---
+++ Sprout/entity/Hotel.java
@@ -0,0 +1,20 @@
+package org.aminesidki.morogo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String location;
+    private Long totalRooms;
+}
